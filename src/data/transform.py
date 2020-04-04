4de4526264e9ddb72cea807854c08ca45ef9a026
fzaussin <felix.zaussinger@gmail.com>
2020-04-04 20:13:27 +0200

    time series data in \processed stored based on FIPS codes from now on. they are unique and thus perfect for mapping to the names.

--- src/data/transform.py
+++ src/data/transform.py
@@ -27,24 +27,24 @@ def csse_main(fpath_in="data/raw/csse", fpath_out="data/processed/csse"):
             # read csv
             file_path = os.path.join(file_dir, file)
             ts_raw = pd.read_csv(file_path, infer_datetime_format=True)
+            ts_raw = ts_raw.convert_dtypes()
 
             # drop all cols apart from Province_States and the time series data
             ancillary_cols = ['Unnamed: 0', 'UID', 'iso2', 'iso3', 'code3',
-                              'FIPS', 'Admin2', 'Country_Region', 'Lat',
-                              'Long_', 'Province_State']
+                              'Admin2', 'Country_Region', 'Lat',
+                              'Long_', 'Province_State', 'Combined_Key']
             if 'Population' in ts_raw.columns:
                 ancillary_cols.append('Population')
 
             # split into time series and ancillary data per state
             ts_clean = (ts_raw.drop(columns=ancillary_cols)
-                        .set_index('Combined_Key')
-                        .transpose()
-                        )
+                        .set_index('FIPS')
+                        .transpose())
             # to datetime index
             ts_clean.index = pd.to_datetime(ts_clean.index, format='%m/%d/%y')
 
             # ancillary data
-            ancillary_cols.append('Combined_Key')
+            ancillary_cols.append('FIPS')
             ancillary_clean = (ts_raw[ancillary_cols]
                                .drop(columns=['Unnamed: 0']))
 
