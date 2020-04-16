be9f59c6e1eb9a65e19acb485ada272371830424
fzaussin <felix.zaussinger@gmail.com>
2020-04-16 11:23:57 +0200

    pipeline run. created csse reader. added notebook to test xarray dataset objects for our purposes.

--- src/data/reader.py
+++ src/data/reader.py
@@ -2,4 +2,21 @@ import os
 import pandas as pd
 
 
-# TODO: create reader for CSSE data
+def read_csse(path):
+    """
+    Preliminary reader for CSSE time series data.
+    Parameters
+    ----------
+    path : str
+        path to .csv file
+
+    Returns
+    -------
+    df : pd.DataFrame
+        CSSE timeseries data.
+            Cols: county FIPS codes
+            Rows: time
+    """
+    df = pd.read_csv(path, index_col=[0])
+    df.index = pd.to_datetime(df.index)
+    return df
