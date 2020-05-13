c3f46b5a874502fa12605aee9ed1f6e9e0e6b7e5
fzaussin <felix.zaussinger@gmail.com>
2020-05-13 14:28:22 +0200

    add statistics.py for useful stats functions. added function to calculate correlation matrix with p-values. further work on correlation of trump/biden sentiments with covid19 data.

---
+++ src/utils/statistics.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import pearsonr
+
+
+def correlation_matrix(df):
+    """
+    Given a pd.DataFrame, calculate Pearson's R
+    and the p-values of the correlation.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe containing numeric values.
+    Returns
+    -------
+    (correlations, p_values) : tuple
+        Tuple of dataframes.
+    """
+    df = df.dropna()._get_numeric_data()
+    cols = pd.DataFrame(columns=df.columns)
+
+    correlations = cols.transpose().join(cols, how='outer')
+    p_values = cols.transpose().join(cols, how='outer')
+
+    for r in df.columns:
+        for c in df.columns:
+            # pearsonr returns a tuple like (corr, pval)
+            correlations[r][c] = round(pearsonr(df[r], df[c])[0], 4)
+            p_values[r][c] = round(pearsonr(df[r], df[c])[1], 4)
+
+    return (correlations.apply(pd.to_numeric),
+            p_values.apply(pd.to_numeric))
